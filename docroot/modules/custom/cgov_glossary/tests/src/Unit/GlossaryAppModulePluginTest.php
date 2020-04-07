fe5f95f5c3c41612802853fce7e78178ffabef96
Sarina <sarinapadilla@users.noreply.github.com>
2020-04-07 11:45:35 -0400

    (#2694, #2692, #2693) cgov_glossary app module creation/integration
    * Add cgov_glossary app module
    * Add cgov_glossary app module plugin
    * Add cgov_glossary route builders: default, expand, search, definition
    * Add cgov_glossary to installed modules for cgov_site
    * Add installation of cgov_glossary to cgov_application_page install
    * Add templates for default, expand, search, definition pages
    * Add YAML content for terms, Spanish terms, and genetics dictionaries
    * Closes #2694
    
    * Added package, entrypoint and config for glossary app
    * Updated glossary template, updated reference to glossary config object
    * Updated glossary app package to beta version 0.13
    * Closes #2692
    * Closes #2693

---
+++ docroot/modules/custom/cgov_glossary/tests/src/Unit/GlossaryAppModulePluginTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\Tests\cgov_glossary\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Tests for the PathProcessorAppModule.
+ */
+class GlossaryAppModulePluginTest extends UnitTestCase {
+
+  /**
+   * Tests GlossaryPlugin functions.
+   */
+  public function testGlossaryPlugin() {
+    $this->assertTrue(TRUE);
+  }
+
+}
